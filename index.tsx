d63718821c5f244fdb160e306af6422b497aaaef
leoblueo888 <leoblueo888@gmail.com>
2026-02-17 15:56:57 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App'; 
 
-// Đoạn code này sẽ tìm cái thẻ <div id="root"> trong file HTML của ông 
-// và đổ toàn bộ giao diện học tiếng Việt vào đó.
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
