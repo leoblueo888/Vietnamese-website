09e054065d00aa978119fb342ecd326ac764c618
leoblueo888 <leoblueo888@gmail.com>
2026-02-18 22:57:27 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Vietnamese-website/', // TÊN REPO CỦA ÔNG PHẢI KHỚP Ở ĐÂY
+  // Tự động điều chỉnh đường dẫn dựa trên môi trường deploy
+  base: process.env.VERCEL ? '/' : '/Vietnamese-website/',
 })
