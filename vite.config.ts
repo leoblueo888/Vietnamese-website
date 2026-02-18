3bd0721c3f70150e4481135d71906727c60d7c0b
leoblueo888 <leoblueo888@gmail.com>
2026-02-18 14:40:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Đảm bảo tên repo chính xác là Vietnamese-website (viết hoa chữ V)
 export default defineConfig({
   plugins: [react()],
-  base: '/Vietnamese-website/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  base: '/Vietnamese-website/', // Thêm dòng này để định nghĩa đúng đường dẫn
 })
