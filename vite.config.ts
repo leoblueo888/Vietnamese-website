52767ee199264b47b0138d038fe138cc28dd4845
leoblueo888 <leoblueo888@gmail.com>
2026-02-18 14:44:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Vietnamese-website/', // Thêm dòng này để định nghĩa đúng đường dẫn
+  base: '/Vietnamese-website/', // Đây là dòng cực kỳ quan trọng để hết trang trắng
 })
